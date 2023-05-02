b110e619982b1e834467ecd396557164958d06b0
andreamastro98 <andreamastrovito98@gmail.com>
2023-05-02 15:29:55 +0200

    esercizio completato

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,22 @@
+<script>
+
+    export default{
+        name: "AppTitle",
+        data() {
+            return {
+                message: 'La mia prima app con Vite!'
+            }
+        }
+    }
+
+    </script>
+
+    <template>
+        <div>
+            <h1>{{ message }}</h1>
+        </div>
+    </template>
+
+    <style scoped>
+
+    </style>
